e877d2c1caf54fb48cdf4c966f53cc3d51076b0b
Robin Hill <robin_hill@web.de>
2022-10-22 10:37:17 +0200

    performance & readability

--- Dotplot.cpp
+++ Dotplot.cpp
@@ -32,71 +32,76 @@ Matrix<char> Dotplot::Perform(std::string seqA, std::string seqB)
 	const auto lastRow = numRows - 1;
 	const auto lastCol = numCols - 1;
 
-	char Marker = 88;
-	char downgrade = 46;
+	char Marker		 = 88;
+	char downgrade	 = 46;
 	char permutation = 112;
 
 	// Fill Hits
-	for (unsigned int i=0; i < numRows; i++){ 
-		for (unsigned j=0; j < numCols; j++){		
+	for (unsigned int i = 0; i < numRows; i++) { 
+		for (unsigned j = 0; j < numCols; j++) {	
+
 			if(seqB[i]==seqA[j]) 
-				D.SetValue(i,j,Marker);		
+				D.SetValue(i, j, Marker);		
 		}
 	}
 	// Remove Fog
-	for (unsigned int i=0; i < numRows; i++){ 
-		for (unsigned j=0; j < numCols; j++){
+	for (unsigned int i = 0; i < numRows; i++) { 
+		for (unsigned j = 0; j < numCols; j++) {
 
 			const auto Dat = D.At(i, j);
 			const auto notDat = !D.At(i, j);
 
-			if (Dat == Marker) {
-
+			if (Dat != Marker) {
+				continue;
+			}
+			else {
 				// useless Corners
-				if (i==0 && j== lastCol || (i== lastRow && j==0)){
-					D.SetValue(i,j,downgrade); 
+				if (i == 0 && j == lastCol || (i == lastRow && j == 0)) {
+					D.SetValue(i, j, downgrade);
 					continue;
 				}
 				// Top & Left
-				if ((i==0 && j < lastCol) || j==0 && i < lastRow){
+				if ((i == 0 && j < lastCol) || j == 0 && i < lastRow) {
 
-					if (Dat != D.At(i+1,j+1)){
-						D.SetValue(i,j, downgrade);
+					if (Dat != D.At(i + 1, j + 1)) {
+						D.SetValue(i, j, downgrade);
 						continue;
 					}
 				}
 				// Right & Bottom 
-				if (i== lastRow || (j == lastCol) && (i&&j)){
+				if (i == lastRow || (j == lastCol) && (i && j)) {
 
-					if (Dat != D.At(i-1,j-1)){
-						D.SetValue(i,j, downgrade);
+					if (Dat != D.At(i - 1, j - 1)) {
+						D.SetValue(i, j, downgrade);
 						continue;
 					}
 				}
 				// Mid
-				if ((i && j >= 1) && (i < lastRow && (j < lastCol))){
+				if ((i && j >= 1) && (i < lastRow && (j < lastCol))) {
 
-					if (notDat == (D.At(i-1,j-1) || D.At(i+1,j+1))){
-						D.SetValue(i,j, downgrade);
+					if (notDat == (D.At(i - 1, j - 1) || D.At(i + 1, j + 1))) {
+						D.SetValue(i, j, downgrade);
 					}
 				}
 			}
 		}
 	} 
 	//Permutation
-	for (unsigned int i = 0; i < numRows; i++){ 
-		for (unsigned j = 0; j < numCols; j++){
+	for (unsigned int i = 0; i < numRows; i++) { 
+		for (unsigned j = 0; j < numCols; j++) {
 
-			if (D.At(i, j) == downgrade){
-
+			if (D.At(i, j) != downgrade) {
+				continue;			
+			}
+			else {
 				// useless Corners
-				if ((i==0 && j == 0) || (i == lastRow && j == lastCol)){
+				if ((i == 0 && j == 0) || (i == lastRow && j == lastCol)) {
 					continue;
-				} 
+				}
 				// Top & Right
-				if (i < lastRow && j >= 1){
-					if (D.At(i+1,j-1) == downgrade){
-						if (!Marker == (D.At(i + 1, j) || D.At(i, j - 1))){
+				if (i < lastRow && j >= 1) {
+					if (D.At(i + 1, j - 1) == downgrade) {
+						if (!Marker == (D.At(i + 1, j) || D.At(i, j - 1))) {
 							D.SetValue(i, j, permutation);
 							continue;
 						}
@@ -104,23 +109,23 @@ Matrix<char> Dotplot::Perform(std::string seqA, std::string seqB)
 				}
 				// Bottom & Left  
 				if (i >= 1 && j < lastCol) {
-					if (D.At(i - 1, j + 1) == permutation){
+					if (D.At(i - 1, j + 1) == permutation) {
 						D.SetValue(i, j, permutation);
 					}
-				}			
+				}
 			}
 		}
 	}
 	//Permutation Fix -slow
-	for (unsigned int i = 0; i < numRows; i++){
-		for (unsigned j = 0; j < numCols; j++){
+	for (unsigned int i = 0; i < numRows; i++) {
+		for (unsigned j = 0; j < numCols; j++) {
 
-			if ((D.At(i, j) == permutation) && (i >=2 && j < lastCol-1)){
+			if ((D.At(i, j) == permutation) && (i >=2 && j < lastCol-1)) {
 				// false downgraded in (//Top & Right) 
-				if (D.At(i - 2, j + 2) == downgrade){
+				if (D.At(i - 2, j + 2) == downgrade) {
 					D.SetValue(i - 2, j + 2, permutation);
 				// remove false positives
-					if(D.At(i - 1, j + 1) != permutation){
+					if(D.At(i - 1, j + 1) != permutation) {
 						D.SetValue(i - 2, j + 2, downgrade);
 					}
 				}
