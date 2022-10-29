55856d3b6ff168c060febd002dd4cdd0cc4d780e
Robin Hill <robin_hill@web.de>
2022-10-29 16:57:02 +0200

    waterman done

--- SmithWaterman.cpp
+++ SmithWaterman.cpp
@@ -24,14 +24,106 @@ SmithWaterman::SmithWaterman(std::string seqA, std::string seqB)
 	Z.SetSize(sequenceB.size() + 1, sequenceA.size() + 1);
 }
 
+int SmithWaterman::Score(char a, char b) {
+
+	if (a == '-' || b == '-') return gapPenalty;
+	if (a == b) return match;
+	else return mismatch;
+}
+
+int SmithWaterman::max(int up, int diag, int left) {
+
+	if (up >= diag) { // >=
+		if (up >= left) return up;
+		else return left;
+	}
+	else {
+		if (diag >= left) return diag;
+		else return left;
+	}
+}
+
+int SmithWaterman::charMax(int up, int diag, int left) {
+
+	if (up >= diag) { // >=
+		if (up >= left) return 'U';
+		else return 'L';
+	}
+	else {
+		if (diag >= left) return 'D';
+		else return 'L';
+	}
+}
+
+int SmithWaterman::checkNegative(int u, int d, int l) {
+
+	if (max(u, d, l) <= 0) return 0;
+	else return max(u, d, l);
+}
+
+void SmithWaterman::FillMats() {
+
+	for (unsigned int i = 1; i < F.GetNumRows(); i++) {
+		for (unsigned j = 1; j < F.GetNumCols(); j++) {
+
+			int up = F.At(i - 1, j)		  + Score(sequenceB[i], '-');
+			int diag = F.At(i - 1, j - 1) + Score(sequenceB[i - 1], sequenceA[j - 1]);
+			int left = F.At(i, j - 1)	  + Score('-', sequenceA[j]);
+
+			F.SetValue(i, j, checkNegative(up, diag, left));
+
+			if (F.At(i, j) > maxVal) {
+				maxVal = F.At(i, j);
+				iMax = i; 
+				jMax = j;
+			}
+
+			if (checkNegative(up, diag, left) != 0) {
+				Z.SetValue(i, j, charMax(up, diag, left));
+			}
+			else Z.SetValue(i, j, '0');
+		}
+	}
+}
+
+void SmithWaterman::Traceback() {
+
+	Z.SetValue(0, 0, '0');
+	for (unsigned i = 1; i < Z.GetNumRows(); i++) {
+		Z.SetValue(i, 0, '0');
+	}
+	for (unsigned j = 1; j < Z.GetNumCols(); j++) {
+		Z.SetValue(0, j, '0');
+	}
+
+	while (F.At(iMax, jMax) > 0) {
+
+		if (Z.At(iMax, jMax) == 'D') {
+			resultA.insert(resultA.begin(), sequenceA[jMax - 1]);
+			resultB.insert(resultB.begin(), sequenceB[iMax - 1]);
+			iMax--; jMax--;
+		}
+		else if (Z.At(iMax, jMax) == 'L') {
+			resultA.insert(resultA.begin(), sequenceA[jMax - 1]);
+			resultB.insert(resultB.begin(), '-');
+			jMax--;
+		}
+		else if (Z.At(iMax, jMax) == 'U') {
+			resultA.insert(resultA.begin(), '-');
+			resultB.insert(resultB.begin(), sequenceB[iMax - 1]);
+			iMax--;
+		}
+	}
+}
+
 AlignedData SmithWaterman::PerformAlignment(int const& match, int const& mismatch, int const& gapPenalty, int const& gapExtensionPenalty)
 {
 	this->match = match;
 	this->mismatch = mismatch;
 	this->gapPenalty = gapPenalty;
 	this->gapExtensionPenalty = gapExtensionPenalty;
-
-	
+	FillMats();
+	Traceback();
 
 	return AlignedData{ F, Z, resultA, resultB };
 }
